2ca775797330d2ba293d03b8f83eb1f8941bb555
Sina <rashidiansina@gmail.com>
2013-06-25 00:47:06 +0430

    sag

--- src/Distributions.java
+++ src/Distributions.java
@@ -1,31 +1,36 @@
-
 public class Distributions {
 
+	
+	public static void main(String[] args) {
+		double d = 0.05 ;
+		double sum = 0;
+		for (int i = 0; i < 60; i++) {
+			double t = ExponentialRandomNumber(d);
+			sum += t;
+			System.out.println(t);
+		}
+		
+		System.out.println(sum/100);
+		
+	}
+	
+	
 	public static int PoissonRandomNumber(double lambda){
 		RandomGenerator rand = new RandomGenerator();
-		int k=0;                          
-		int max_k = 1000;           
-		double p = rand.nextRand(); 
-		double P = Math.exp(-lambda);          
-		double sum = P;                     
-
-		if (sum >= p)
-			return 0;             
 		
-		for(k=1; k < max_k;++k) {         
-			P*=lambda/(double)k;            
-			sum+=P;                         
-			if (sum>=p)
-				break;              
-		}
-		return k;                         
+		double l = Math.pow(Math.E, -lambda)  , p = 1;
+		int k = 0;
+		do{
+			k++;
+			p = rand.nextRand() * p;
+		}while(p > l);
+		return k-1;
 	}
 	
 	public static double ExponentialRandomNumber(double lambda){
 		RandomGenerator rand = new RandomGenerator();
-		double t;
 		double x = rand.nextRand();
-		double numinator = -(naturalLog(x) * x);
+		double numinator = -(naturalLog(x) );
 		return numinator/lambda; 
 	}
 	
